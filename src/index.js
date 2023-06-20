faa69ef58ad669c876996abe01aa566369fba97a
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-06-20 15:29:02 +0300

    Шестой шаг

--- src/index.js
+++ src/index.js
@@ -7,18 +7,6 @@ const getExtension = (filename) => path.extname(filename).slice(1);
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-const makeStr = (obj) => {
-  const { key, value, type } = obj;
-  switch (type) {
-    case 'added':
-      return `  + ${key}: ${value}\n`;
-    case 'deleted':
-      return `  - ${key}: ${value}\n`;
-    default:
-      return `    ${key}: ${value}\n`;
-  }
-};
-
 const generateDiff = (filepath1, filepath2) => {
   const pathFile1 = getPath(filepath1);
   const pathFile2 = getPath(filepath2);
@@ -32,26 +20,32 @@ const generateDiff = (filepath1, filepath2) => {
   const keysData1 = Object.keys(data1);
   const keysData2 = Object.keys(data2);
 
-  const diffList = _.sortBy(_.uniq([...keysData1, ...keysData2]))
-    .flatMap((key) => {
-      if (!Object.hasOwn(data1, key)) {
-        return { key, value: data2[key], type: 'added' };
-      }
-      if (!Object.hasOwn(data2, key)) {
-        return { key, value: data1[key], type: 'deleted' };
-      }
-      if (data1[key] === data2[key]) {
-        return { key, value: data1[key], type: 'unchanged' };
-      }
-      return [{ key, value: data1[key], type: 'deleted' }, { key, value: data2[key], type: 'added' }];
-    })
-    .reduce((acc, diff) => {
-      let newAcc = acc;
-      newAcc += makeStr(diff);
-      return newAcc;
-    }, '');
+  const keys = _.sortBy(_.uniq([...keysData1, ...keysData2]));
+  
+  const tree = keys.map((key) => {
 
-  return `{\n${diffList}}`;
-};
+    const makeDiff = (obj1, obj2) => {
+      if (!Object.hasOwn(obj1, key)) {
+        return { key, value: obj2[key], type: 'added'};
+      }
+      if (!Object.hasOwn(obj2, key)) {
+        console.log(obj1[key])
+        return { key, value: obj1[key], type: 'deleted'};
+      }
+      if (obj1[key] === obj2[key]) {
+        console.log(obj1[key])
+        return {key, value: obj1[key], type: 'unchanged'}
+      }
+      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        return { key, value: makeDiff(obj1[key], obj2[key]), type: 'nested'}
+      }
+      return { key, oldValue: obj1[key], newValue: obj2[key], type: 'changed'}
+    }
+    return makeDiff(data1, data2);
+  })
+  return tree;
+  }
+
+console.log(generateDiff('file1.json', 'file2.json'));
 
 export default generateDiff;
